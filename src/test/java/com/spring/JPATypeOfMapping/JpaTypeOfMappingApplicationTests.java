5f61e998f3c893482a674973aef4322b4a686c7b
KamalakannanKKK <109393676+KamalakannanKKK@users.noreply.github.com>
2024-04-28 14:04:54 +0530

    Create JpaTypeOfMappingApplicationTests.java

---
+++ src/test/java/com/spring/JPATypeOfMapping/JpaTypeOfMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.JPATypeOfMapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTypeOfMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
